b01f8c1ccf3d9152190f5e216f48d261df49d32d
Robert Howerton <wh01683@georgiasouthern.edu>
2015-03-11 12:41:54 -0400

    Create RandomBooleanGenerator.java

---
+++ RandomBooleanGenerator.java
@@ -0,0 +1,15 @@
+package BankApplication;
+
+import java.util.Random;
+public class RandomBooleanGenerator {
+
+	private Random r = new Random();
+	
+	public boolean getRandomBoolean(){
+		float temp = r.nextFloat();
+		if (temp < .50) return false;
+		else
+			return true;
+	}
+	
+}
