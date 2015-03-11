ea24da64c765275e6ad4fdf987c6b73c4e53af01
Robert Howerton <wh01683@georgiasouthern.edu>
2015-03-11 12:41:06 -0400

    Create Bank.java

---
+++ Bank.java
@@ -0,0 +1,16 @@
+package BankApplication;
+
+public class Bank {
+
+	private double balance;
+	
+	public Bank(double newBalance){
+		this.balance = newBalance;}
+	
+	public void setBalance(double newBalance){
+		this.balance = newBalance;}
+	
+	public double getBalance(){
+		return this.balance;}
+	
+}
