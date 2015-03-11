47dd1d55a8d045b37f62403f3947f7348443ef69
Robert Howerton <wh01683@georgiasouthern.edu>
2015-03-11 15:45:09 -0400

    restructured to be pro-er

--- Customer.java
+++ Customer.java
@@ -1,23 +1,47 @@
 package BankApplication;
-
 import java.util.Random;
-public class Customer {
+import java.util.UUID;
 
-	
+
+public class Customer {
+	private MoBettaScanner nameS = new MoBettaScanner("Please enter your name: ", 2, 50);
+	private MoBettaScanner ageS = new MoBettaScanner("Please enter your age: ", 14, 99);
+	private MoBettaScanner latePayments = new MoBettaScanner("Please enter total number of late payments you've made, if any: ", -1, 101);
+	private MoBettaScanner startBalance = new MoBettaScanner("Please enter your starting balance: ", 1, 2000000000.0);
+	private MoBettaScanner credInquiries = new MoBettaScanner("Please enter the number of recent credit inquiries: ", -1, 99);
+	private MoBettaScanner credBalance = new MoBettaScanner("Please enter your current outstanding credit card balance.", -1, 2000000000.0);
+	private MoBettaScanner credHistory = new MoBettaScanner("Please enter the length of your credit history in years: ", -1, 99);
+	private MoBettaScanner credLim = new MoBettaScanner("Please enter your total credit limit.", -1.0, 2000000000.0);
 	Random r = new Random();
-	private final String CUSTOMER_ID;
-	private String name;
+	
+	private final UUID CUSTOMER_ID;
+	
+	final String HEX_ALPHABET = "abcdefABCDEF0123456789";
+	final int NUM_IN_HEX = HEX_ALPHABET.length();
+	private final String name;
+	
+	private CreditReport _cred;
+	private ChexSystems _score;
+	
 	private int age;
-	private ChexSystems _score = new ChexSystems();
 	private int ChexSystemsScore = 0; //intialize to 0 indicating no prior history
 	
-	public Customer(String newName, int newAge){
-		this.CUSTOMER_ID = custIDGen(10);
-		this.age = newAge;
-		this.name = newName;
-		if (newAge > 17) //if age is 18 or greater, generates random score for the customer
-			this.ChexSystemsScore = _score.getScore();}
 	
+	public Customer(){
+		this.CUSTOMER_ID = new UUID(64,64);
+		this.age = ageS.intGet();
+		this.name = nameS.stringGet();
+		if(this.age>17)
+			this._cred = fillCredReport(this.age);
+		this._score = new ChexSystems();
+				
+		}
+
+	
+	
+	public UUID getUUID(){
+		return this.CUSTOMER_ID;
+	}
 	public int getAge(){
 		return this.age;}
 	
@@ -25,16 +49,28 @@ public class Customer {
 		return this.name;}
 	
 	public int getChexSystemsScore(){
-		return this.ChexSystemsScore;}
+		return this._score.getScore();}
 	
-	private String custIDGen(int length){
-		String temp = "";
-		final String HEX_ALPHABET = "abcdefABCDEF0123456789";
-		final int NUM_IN_HEX = HEX_ALPHABET.length();
-		for(int i = 0; i < length; i++){
-			temp+=(HEX_ALPHABET.charAt(r.nextInt(NUM_IN_HEX)));
-		}
+	public int getCreditScore(){
+		return this._cred.getCreditScore();
+	}
+	
+	private CreditReport fillCredReport(int tempAge){
 		
-		return temp;}
-	
+		int age = tempAge;
+		System.out.println("Since you are " + tempAge + " years old, you must provide some credit information.");
+		double credLimit = credLim.doubleGet();
+		double accountBalance = credBalance.doubleGet();
+		int lenCredHistory = credHistory.intGet();
+		int latePaymentsOnRecord = latePayments.intGet();
+		int recentCredInquiries = credInquiries.intGet();
+		
+		CreditReport tempcr = new CreditReport(age, latePaymentsOnRecord, recentCredInquiries, credLimit,
+				accountBalance, lenCredHistory);
+		
+		
+		return tempcr;
+		}
+
+
 }
